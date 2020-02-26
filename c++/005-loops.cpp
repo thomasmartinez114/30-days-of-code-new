2f8e67854d094347aa95e2e47b4493980273b26a
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-26 00:40:37 -0500

    completed Day 005 - loops

---
+++ c++/005-loops.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i < 11; i++)
+    {
+        printf("%i x %i = %i\n", n, i, n*i);
+    }
+
+    return 0;
+}
