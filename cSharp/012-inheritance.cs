fc52a6a7909a0e3189668c4c40c945f46c031bc3
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-02 19:59:20 -0500

    need to finish

---
+++ cSharp/012-inheritance.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+
+class Person{
+	protected string firstName;
+	protected string lastName;
+	protected int id;
+	
+	public Person(){}
+	public Person(string firstName, string lastName, int identification){
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.id = identification;
+	}
+	public void printPerson(){
+		Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id); 
+	}
+}
+
+class Student : Person{
+    private int[] testScores;  
+  
+    /*	
+    *   Class Constructor
+    *   
+    *   Parameters: 
+    *   firstName - A string denoting the Person's first name.
+    *   lastName - A string denoting the Person's last name.
+    *   id - An integer denoting the Person's ID number.
+    *   scores - An array of integers denoting the Person's test scores.
+    */
+    // Write your constructor here
+    constructor(string firstName, string lastName, int id, int [] scores)
+    {
+        base(firstName, lastName, id)
+        this.allScores = scores;
+    }
+    
+    /*	
+    *   Method Name: Calculate
+    *   Return: A character denoting the grade.
+    */
+    // Write your method here
+    static void Calculate()
+    {
+        
+    }
+
+}
