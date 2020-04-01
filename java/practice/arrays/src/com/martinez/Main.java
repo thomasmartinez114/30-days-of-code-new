913dc90dfecbb060649439cb0ad36a5b4a09eb4a
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-31 23:41:38 -0400

    loop through array to find largest score

--- java/practice/arrays/src/com/martinez/Main.java
+++ java/practice/arrays/src/com/martinez/Main.java
@@ -5,19 +5,15 @@ public class Main {
     public static void main(String[] args) {
         // init the array
         double grades[] = {96.5, 98.7, 99.3, 88.4, 88.6, 90.1};
-        int numbers[] = new int[10];
+        double max;
+
+        max = grades[0];
 
         // Loop through
         for (int i = 0; i <= 5; i++) {
-            System.out.println("Grade number " + i + " is " + grades[i]);
+            if (grades[i] > max)
+                max = grades[i];
         }
-
-        for (int i = 0; i <= 9; i++){
-            numbers[i] = i + 10;
-        }
-
-        for(int i = 0; i <= 9; i++){
-            System.out.println(numbers[i]);
-        }
+        System.out.println("The max grade in the class is: " + max);
     }
 }
