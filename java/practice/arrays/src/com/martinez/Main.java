08068ccb7c90a42e64f2cdde5ebbc84c419ae6fb
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-31 23:36:16 -0400

    init new numbers[] and sout the indexes

--- java/practice/arrays/src/com/martinez/Main.java
+++ java/practice/arrays/src/com/martinez/Main.java
@@ -5,10 +5,19 @@ public class Main {
     public static void main(String[] args) {
         // init the array
         double grades[] = {96.5, 98.7, 99.3, 88.4, 88.6, 90.1};
+        int numbers[] = new int[10];
 
         // Loop through
         for (int i = 0; i <= 5; i++) {
             System.out.println("Grade number " + i + " is " + grades[i]);
+        }
+
+        for (int i = 0; i <= 9; i++){
+            numbers[i] = i + 10;
+        }
+
+        for(int i = 0; i <= 9; i++){
+            System.out.println(numbers[i]);
         }
     }
 }
