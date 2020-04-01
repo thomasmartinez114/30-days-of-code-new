f83c08ee5aacc4a0cd21a319263394675d6c0353
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-31 23:06:45 -0400

    Loop through the grades

--- java/practice/arrays/src/com/martinez/Main.java
+++ java/practice/arrays/src/com/martinez/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
         // init the array
         double grades[] = {96.5, 98.7, 99.3, 88.4, 88.6, 90.1};
+
+        // Loop through
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(grades[i]);
+        }
     }
 }
